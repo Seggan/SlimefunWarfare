d3a1c2d7c1d2cff668b9b65a1606dd405a66b011
Seggan <segganew@gmail.com>
2021-07-19 21:38:23 -0400

    Configs

--- src/main/java/io/github/seggan/slimefunwarfare/machines/MeteorAttractor.java
+++ src/main/java/io/github/seggan/slimefunwarfare/machines/MeteorAttractor.java
@@ -1,5 +1,6 @@
 package io.github.seggan.slimefunwarfare.machines;
 
+import io.github.mooy1.infinitylib.configuration.AddonConfig;
 import io.github.mooy1.infinitylib.players.CoolDownMap;
 import io.github.seggan.slimefunwarfare.SlimefunWarfare;
 import io.github.seggan.slimefunwarfare.lists.Categories;
@@ -20,7 +21,9 @@ import javax.annotation.Nonnull;
 
 public class MeteorAttractor extends SimpleSlimefunItem<BlockUseHandler> {
 
-    private static final CoolDownMap cooldowns = new CoolDownMap(1000 * 60);
+    private static final CoolDownMap cooldowns = new CoolDownMap(1000L * 60L *
+        SlimefunWarfare.inst().getConfig().getInt("space.attractor-cooldown", 1)
+    );
 
     public MeteorAttractor() {
         super(Categories.MACHINES, Items.METEOR_ATTRACTOR, RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[]{
@@ -34,15 +37,19 @@ public class MeteorAttractor extends SimpleSlimefunItem<BlockUseHandler> {
         Block b = l.getBlock();
         if (!(BlockStorage.check(l) instanceof MeteorAttractor)) return;
 
-        int x = ThreadLocalRandom.current().nextInt(b.getX() - 100, b.getX() + 101);
-        int z = ThreadLocalRandom.current().nextInt(b.getZ() - 100, b.getZ() + 101);
+        AddonConfig config = SlimefunWarfare.inst().getConfig();
+
+        int radius = config.getInt("space.meteor-drop-radius", 100);
+
+        int x = ThreadLocalRandom.current().nextInt(b.getX() - radius, b.getX() + radius + 1);
+        int z = ThreadLocalRandom.current().nextInt(b.getZ() - radius, b.getZ() + radius + 1);
         World world = b.getWorld();
 
         world.createExplosion(x, world.getHighestBlockYAt(x, z), z, 4);
 
         SlimefunWarfare.inst().runSync(() -> {
             SlimefunItemStack stack = Items.OSMIUM_METEOR;
-            if (ThreadLocalRandom.current().nextInt(100) < SlimefunWarfare.inst().getConfig().getInt("space.segganesson-chance", 0, 100)) {
+            if (ThreadLocalRandom.current().nextInt(100) < config.getInt("space.segganesson-chance", 0, 100)) {
                 stack = Items.SEGGANESSON_METEOR;
             }
 
@@ -56,14 +63,23 @@ public class MeteorAttractor extends SimpleSlimefunItem<BlockUseHandler> {
     @Override
     public BlockUseHandler getItemHandler() {
         return (b) -> {
+            AddonConfig config = SlimefunWarfare.inst().getConfig();
             if (cooldowns.check(b.getPlayer().getUniqueId())) {
                 cooldowns.reset(b.getPlayer().getUniqueId());
-                int mins = ThreadLocalRandom.current().nextInt(10, 31);
+
+                int mins = ThreadLocalRandom.current().nextInt(
+                    config.getInt("space.meteor-min-time", 10),
+                    config.getInt("space.meteor-max-time", 30) + 1
+                );
+
                 Location l = b.getClickedBlock().get().getLocation();
                 b.getPlayer().sendMessage("Sending meteor in " + mins + " minutes");
                 SlimefunWarfare.inst().runSync(() -> drop(l), mins * 60 * 20L);
             } else {
-                b.getPlayer().sendMessage(ChatColor.RED + "The Meteor Attractor has a 1 minute cooldown");
+                b.getPlayer().sendMessage(ChatColor.RED + "The Meteor Attractor has a "
+                    + config.getInt("space.space.attractor-cooldown", 1) +
+                    " minute cooldown"
+                );
             }
         };
     }
