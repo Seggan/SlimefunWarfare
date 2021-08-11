6f0b7b99aa6dd978cb66cd46b9f0fb43abe9e1b0
Seggan <segganew@gmail.com>
2021-08-11 09:08:11 -0400

    You can now be using multiple Radios

--- src/main/java/io/github/seggan/slimefunwarfare/listeners/ChatListener.java
+++ src/main/java/io/github/seggan/slimefunwarfare/listeners/ChatListener.java
@@ -51,7 +51,6 @@ public class ChatListener implements Listener {
                         .append(decrypted)
                         .create()
                     );
-                    break;
                 }
             }
         }
