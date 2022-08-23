6889668be4f1fcfe10b06fbaf246a6993e16e772
Seggan <segganew@gmail.com>
2022-08-23 14:11:40 -0400

    Fix #42

--- src/main/java/io/github/seggan/slimefunwarfare/listeners/BreakListener.java
+++ src/main/java/io/github/seggan/slimefunwarfare/listeners/BreakListener.java
@@ -16,7 +16,7 @@ public class BreakListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onStoneBreak(BlockBreakEvent e) {
-        if (e.getBlock().getType() != Material.STONE) return;
+        if (e.getBlock().getType() != Material.STONE || Items.BORAX.getItem().isDisabled()) return;
         if (ThreadLocalRandom.current().nextDouble(100) < boraxChance) {
             e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), Items.BORAX.clone());
         }
